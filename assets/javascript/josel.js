7f952d376579b1116ba028ab30b491feae478cca
Thomas Nicholson <40121607+thomasnicholson@users.noreply.github.com>
2022-08-25 10:15:35 +0200

    remove java folder

--- assets/javascript/josel.js
+++
@@ -1,7 +0,0 @@
-
-
-// Load header and footer after page has loaded
-$(function(){
-    $(".landing-section").load("../../components/navigation.html");
-    $(".footer").load("../../components/footer.html");
-}); 
