07ef27688abc5fbaf80eb8f2dee54db531972ee4
Thomas Nicholson <40121607+thomasnicholson@users.noreply.github.com>
2022-08-25 10:04:02 +0200

    fixed paths

--- docs/assets/javascript/josel.js
+++ docs/assets/javascript/josel.js
@@ -2,7 +2,7 @@
 
 // Load header and footer after page has loaded
 $(function(){
-    $(".landing-section").load("/components/navigation.html");
-    $(".footer").load("/components/footer.html");
+    $(".landing-section").load("sethjosel/components/navigation.html");
+    $(".footer").load("sethjosel/components/footer.html");
     // document.getElementsByClassName("current-year").innerHTML = new Date().getFullYear();
 }); 
